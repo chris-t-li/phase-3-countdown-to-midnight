940345adfc5ab30017569d8abe4d95399a27f520
Chris Li <chris.lty07@gmail.com>
2022-10-31 13:33:12 -0400

    commit tree ef982fcb5842c7b34270af3b95840bd52605e8de

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+    while integer >= 1
+        puts "#{integer} SECOND(S)!"
+        integer -=1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+# puts countdown(10)
+def countdown_with_sleep(number)
+    while number >= 1
+        puts "#{number} SECOND(S)!"
+        number -= 1
+        sleep 1
+    end
+    "HAPPY NEW YEAR!"
 end
